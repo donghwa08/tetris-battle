5eeb7f777de621f2e05b8f5c4a91b330f28e9c42
24s102h0659-spec <24s102h0659@sonline20.sen.go.kr>
2026-06-25 09:42:26 +0900

    fix swap and boardUpdate

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,11 +5,14 @@ const jwt = require("jsonwebtoken");
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // 회원가입
--- server/routes/score.js
+++ server/routes/score.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 router.post("/score", (req, res) => {
